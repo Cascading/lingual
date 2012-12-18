16178417d0d843ebe2068b4e50997c1f36524db8
Chris K Wensel <chris@wensel.net>
2012-12-18 07:53:57 -0800

    refactored build to isolate dependencies into fatjars, which can be used as self contained jdbc driver jars

--- lingual-client/src/dist/bin/lingual.sh
+++ lingual-client/src/dist/bin/lingual.sh
@@ -25,7 +25,7 @@ JAVA_EXEC=`which java`
 
 [ ! -e $JAVA_EXEC ] && echo "could not find java, check JAVA_HOME!!" && exit -1
 
-LIBS=$BASEDIR/lib
+LIBS="$BASEDIR/lib/*"
 
 # get arguments
 COMMAND=$1
@@ -35,14 +35,46 @@ if [ "$COMMAND" = "shell" ] ; then
   MAIN=cascading.lingual.shell.Shell
 elif [ "$COMMAND" = "catalog" ] ; then
   MAIN=cascading.lingual.catalog.Catalog
+else
+  echo "ERROR: unknown command: $COMMAND"
+  exit 1
 fi
+
+PLATFORM=local
+
+ARGS="$@"
+
+while [ -n "$1" ]
+ do
+     case $1 in
+         --platform)
+             PLATFORM=$2
+             shift 2
+             ;;
+         *)  # no more options. Stop while loop
+             shift
+             ;;
+     esac
+ done
 
 OPTIONS=
 
-HADOOP_CLASSPATH=
+LINGUAL_CLASSPATH="$BASEDIR/platform/$PLATFORM/*"
 
-[ -n "$HADOOP_HOME" ] && . $BINDIR/hadoop-env
+case $PLATFORM in
+   local)
+       ;;
+   hadoop)
+       HADOOP_CLASSPATH=
+       [ -n "$HADOOP_HOME" ] && . $BINDIR/hadoop-env
+       LINGUAL_CLASSPATH="$LINGUAL_CLASSPATH:$HADOOP_CLASSPATH"
+       ;;
+   *)
+       echo "ERROR: Unknown platform: $PLATFORM"
+       exit 1
+       ;;
+esac
 
 #[ -n "$COLUMNS" ] && OPTIONS="--display-width $COLUMNS $OPTIONS"
 
-$JAVA_EXEC -Xmx512m -cp "$LIBS/*:$HADOOP_CLASSPATH" $MAIN $OPTIONS "$@"
+$JAVA_EXEC -Xmx512m $OPTIONS -cp "$LIBS:$LINGUAL_CLASSPATH" $MAIN $ARGS
