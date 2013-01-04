a450fa9c185e3a3d64c227d649fdaeca59818b20
Chris K Wensel <chris@wensel.net>
2013-01-04 15:52:29 -0800

    formatting

--- lingual-core/src/main/java/cascading/lingual/optiq/CascadingCalcRel.java
+++ lingual-core/src/main/java/cascading/lingual/optiq/CascadingCalcRel.java
@@ -76,9 +76,7 @@ public class CascadingCalcRel extends CalcRelBase implements CascadingRelNode
     List<Expression> parameters = new ArrayList<Expression>();
 
     for( int i = 0; i < fields.size(); i++ )
-      {
       parameters.add( Expressions.parameter( fields.getType( i ), fields.get( i ).toString() ) );
-      }
 
     Expressions.FluentList<Statement> statements = Expressions.<Statement>list();
     Expression condition = RexToLixTranslator.translateCondition(
