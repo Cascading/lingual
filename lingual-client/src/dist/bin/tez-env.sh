981332c1b251ac6d4e5719df985dab450faf1e43
Andre Kelpe <efeshundertelf@googlemail.com>
2015-03-09 15:34:20 +0100

    improve tez environment handling robustness

--- lingual-client/src/dist/bin/tez-env.sh
+++ lingual-client/src/dist/bin/tez-env.sh
@@ -1,1 +1,10 @@
+if [ -z "$TEZ_JARS" ]; then
+  echo "please set the TEZ_JARS environment variable"
+  exit 1
+fi
+if [ -z "$TEZ_CONF_DIR" ]; then
+  echo "please set the TEZ_CONF_DIR environment variable"
+  exit 1
+fi
+
 export YARN_CLASSPATH="$YARN_CLASSPATH:${TEZ_CONF_DIR}:${TEZ_JARS}/*:${TEZ_JARS}/lib/*"
